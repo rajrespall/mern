deced3523ee6b9671e2ebfbefbf628a8aac890c6
RajTheGreatest <rajesh.respall@tup.edu.ph>
2024-10-12 14:51:46 +0800

    connect fe and be

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/api/auth", authRoutes) ;
 app.use("/api", productRoutes);
